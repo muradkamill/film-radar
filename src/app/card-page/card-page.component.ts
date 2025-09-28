c92e1eb960734169bbe8a1a0a6e5b9c3711ec656
muradkamil <muradkamilllll@gmail.com>
2025-09-28 16:47:58 +0400

    ..

--- src/app/card-page/card-page.component.ts
+++ src/app/card-page/card-page.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from '../shared/footer/footer.component';
-import { HeaderComponent } from "../shared/header/header.component";
-import { LogoSearchComponent } from "../shared/logo-search/logo-search.component";
 import { InfoComponent } from "./info/info.component";
 
 @Component({
   selector: 'app-card-page',
-  imports: [FooterComponent, HeaderComponent, LogoSearchComponent, InfoComponent],
+  imports: [ InfoComponent],
   templateUrl: './card-page.component.html',
   styleUrl: './card-page.component.css'
 })
 export class CardPageComponent {
-  
+
 }
